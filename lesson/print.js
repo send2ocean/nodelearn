93ad4b431c980a9f1ff46e868f17c8b5bd13b311
send2ocean <send2ocean@foxmail.com>
2016-04-20 09:01:27 +0800

    update print

--- lesson/print.js
+++ lesson/print.js
@@ -3,8 +3,7 @@ var len=2*n-1;
 for(var i=1;i<len+1;i++){
 	var abs = Math.abs(n-i);
 	var str="";
-	for(var j=0;j<len;j++){
-		
+	for(var j=0;j<len;j++){		
 		if(j==abs || Math.abs(len-j-1)==abs){
 			str+="*";
 		}else{
