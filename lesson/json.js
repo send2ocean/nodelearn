f66aed80c8e85918c8410f6734b74ae5cd7a9e65
send2ocean <send2ocean@foxmail.com>
2016-05-25 22:38:03 +0800

    json process

---
+++ lesson/json.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+ 
+var bf = fs.readFileSync('./data/phones/phones.json')
+ 
+var content = bf.toString();
+
+var obj = JSON.parse(content);
+
+obj.push({'abc':'abcabc'})
+
+var str = JSON.stringify(obj);
+
+console.log(str);
+
+	fs.writeFile('./data/phones/phones.json', str, (err) => {
+  if (err) throw err;
+	console.log('It\'s saved!');
+});
